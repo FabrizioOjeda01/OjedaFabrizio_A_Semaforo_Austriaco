4df265463658405eef8dac9a9c25a82684b6e0e1
FabrizioOjeda01 <46572065+FabrizioOjeda01@users.noreply.github.com>
2019-02-03 16:07:51 +0100

    Add files via upload
    Aggiunta del seriale per il numero di lampeggi, la durata dei lampeggi, la durata del verde e la durata del giallo.
    Aggiunta di qualche commento.
    Codice ancora da compattare

--- SemaforoAustriaco/SemaforoAustriaco.ino
+++ SemaforoAustriaco/SemaforoAustriaco.ino
@@ -1,20 +1,24 @@
 int rosso1  = 2;
-int giallo1 = 3;
+int giallo1 = 3;     //Variabili primo semaforo
 int verde1  = 4;
 
 int rosso2  = 8;
-int giallo2 = 9;
+int giallo2 = 9;     //variabili secondo semaforo
 int verde2  = 10;
-int numLampeggi;
 
-void Lampeggia(int led, int ritardo, int tick){
-  for (int i = 0; i <= tick; i++){
-    delay(ritardo);
+int numLampeggi = 0;
+int numTLamp    = 0; //Variabili per il seriale
+int tVerde      = 0;
+int tGiallo     = 0;
+
+void Lampeggia(int led, int numTLamp, int numLampeggi){
+  for (int i = 0; i <= numLampeggi; i++){
+    delay(numTLamp);
     digitalWrite(led, HIGH);
-    delay(ritardo);
+    delay(numTLamp);
     digitalWrite(led, LOW);
   }
-}
+} 
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,12 +28,23 @@ void setup() {
   pinMode(rosso2, OUTPUT);
   pinMode(giallo2, OUTPUT);
   pinMode(verde2, OUTPUT);
-
   Serial.begin(9600);
-
-  Serial.print("quanti lampeggi?");
-  while (Serial.available == 0) {};
+  
+  Serial.println("Quanti lampeggi deve fare il verde?"); //Legge in input quanti lampeggi deve fare il verde
+  while (Serial.available() == 0) {};
   numLampeggi = Serial.readString().toInt();
+
+  Serial.println("Quanto devono durare i lampeggi?");   //Legge in input la durata di ogni lampeggio
+  while (Serial.available() == 0) {};
+  numTLamp = Serial.readString().toInt();
+  
+  Serial.println("Quanto tempo dura il verde?");        //Legge in input quanto tempo dura il verde
+  while (Serial.available() == 0) {};
+  tVerde = Serial.readString().toInt();
+
+  Serial.println("Quanto tempo dura il giallo?");       //Legge in input quanto tempo dura il giallo
+  while (Serial.available() == 0) {};
+  tGiallo = Serial.readString().toInt();
 }
 
 void loop() {
@@ -40,14 +55,14 @@ void loop() {
  digitalWrite (rosso2, HIGH);
  digitalWrite (giallo2, LOW);
  
- delay (2000);
+ delay (tVerde);
 
- Lampeggia(verde1, 350, 4);
+ Lampeggia(verde1, numTLamp, numLampeggi);
  digitalWrite (verde1, LOW);
  digitalWrite (giallo1, HIGH);
  digitalWrite (giallo2, HIGH);
  
- delay (2000);
+ delay (tGiallo);
 
  digitalWrite (rosso2, LOW);
  digitalWrite (giallo1, LOW);
@@ -55,12 +70,12 @@ void loop() {
  digitalWrite (giallo2, LOW);
  digitalWrite (verde2, HIGH);
 
- delay (2000);
+ delay (tVerde);
  
- Lampeggia (verde2, 350, 4);
+ Lampeggia (verde2, numTLamp, numLampeggi);
  digitalWrite (giallo1, HIGH);
  digitalWrite (verde2, LOW);
  digitalWrite (giallo2, HIGH);
  
- delay (2000);
+ delay (tGiallo);
 }
